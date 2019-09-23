addd9847ea882e4d317678dc0cff97cd5a7e4cd5
Dhian <muthidp@gmail.com>
2019-09-23 12:07:35 +0700

    on logout

--- client-sample/src/app.js
+++ client-sample/src/app.js
@@ -150,6 +150,7 @@ app.controller("clientSampleController", ($scope, $interval) => {
     };
 
     $scope.$on('$destroy', function() {
+        $scope.actions.onLogout();
         $scope.actions.destroyInterval();
     });
 
